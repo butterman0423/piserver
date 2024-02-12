48482ddf880035e735d783fce5ef0d384c77d7b9
BUTTER_MAC <butterman423@gmail.com>
2024-02-12 11:36:21 -0500

    Removed <img> tag from entry (editor check reason)

--- app/entry.tsx
+++ app/entry.tsx
@@ -3,8 +3,9 @@ import { EntryInfo } from "src/explorer"
 export default function Entry({ entry }: { entry: EntryInfo }) {
     const { name, extension } = entry;
     
-    return <div>
-        <img></img>
-        <p>{name}.{extension}</p>
-    </div>
+    return ( 
+        <div>
+            <p>{name}.{extension}</p>
+        </div>
+    )
 }
