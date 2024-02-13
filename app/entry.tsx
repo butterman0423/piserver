0786281d778304bf7bdb97929bcead61e70084b5
Nathaniel Escaro <butterman423@gmail.com>
2024-02-13 13:01:47 -0500

    Added a frame for handle click events

--- app/entry.tsx
+++ app/entry.tsx
@@ -1,10 +1,24 @@
+import { ENTRY_TYPE } from "src/explorer";
 import { EntryInfo } from "src/explorer"
+
+function handleClick(ev: React.MouseEvent<HTMLDivElement, MouseEvent>, entry: EntryInfo): void {
+    const {id, type} = entry;
+
+    switch(type) {
+        case ENTRY_TYPE.DIRECTORY:
+            break;
+        case ENTRY_TYPE.REGULAR:
+            break;
+        default:
+            console.error("Error: Unknown Entry Type");
+    }
+}
 
 export default function Entry({ entry }: { entry: EntryInfo }) {
     const { name, extension } = entry;
     
     return ( 
-        <div>
+        <div onClick={(ev) => handleClick(ev, entry)}>
             <p>{name}.{extension}</p>
         </div>
     )
