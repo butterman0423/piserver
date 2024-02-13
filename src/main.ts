26ede4d16f8ebdfdfac7a5f4571a7c916d756978
Nathaniel Escaro <butterman423@gmail.com>
2024-02-13 13:46:07 -0500

    Added comments for basic routes

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import express, { Express, Request, Response } from 'express'
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
+    // This is the default page, it should
+    // redirect to /:id where :id is the 
+    // default root table
+});
 
-})
-
-app.get('/')
+app.get('/:id', (req: Request, res: Response) => {
+    // This will query the db, and change
+    // create a new file explorer.
+});
 
 export default app;
