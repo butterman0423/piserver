12514586c140d2e72cd5ecac173f0eba633eb68e
BUTTER_MAC <butterman423@gmail.com>
2024-02-14 11:10:57 -0500

    Added icon section and grabber

--- app/entry.tsx
+++ app/entry.tsx
@@ -1,7 +1,7 @@
 import { ENTRY_TYPE, EntryInfo } from "src/explorer";
 import strings from "res/strings.json";
 
-function getDestURL(entry: EntryInfo): string {
+function getDestURL(entry: EntryInfo) : string {
     const {id, type} = entry;
     switch(type) {
         case ENTRY_TYPE.DIRECTORY:
@@ -14,11 +14,28 @@ function getDestURL(entry: EntryInfo): string {
     }
 }
 
+function getIconInfo(entry: EntryInfo) : {img_res: string, alt: string} {
+    const { extension } = entry;
+
+    switch(extension) {
+        // TODO
+        default:
+            return {img_res: "", alt: ""};
+    }
+}
+
 export default function Entry({ entry }: { entry: EntryInfo }) {
     const { name, extension } = entry;
+    const { img_res, alt } = getIconInfo(entry);
     
     return (
         <a href={getDestURL(entry)}>
+            <div>
+                <img
+                    src={img_res}
+                    alt={alt}
+                />
+            </div>
             <div>
                 <p>{name}.{extension}</p>
             </div>
