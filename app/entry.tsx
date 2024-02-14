7377a33acaaa10dfcab37bb2828b1582d30870da
Nathaniel Escaro <butterman423@gmail.com>
2024-02-14 00:11:56 -0500

    Directly imported string resources for URL changes

--- app/entry.tsx
+++ app/entry.tsx
@@ -1,13 +1,16 @@
 import { ENTRY_TYPE } from "src/explorer";
 import { EntryInfo } from "src/explorer"
+import strings from "res/strings.json";
 
 function handleClick(ev: React.MouseEvent<HTMLDivElement, MouseEvent>, entry: EntryInfo): void {
     const {id, type} = entry;
 
     switch(type) {
         case ENTRY_TYPE.DIRECTORY:
+            window.location.assign(`${strings.path_explorer_path}${id}`);
             break;
         case ENTRY_TYPE.REGULAR:
+            window.location.assign(`${strings.path_file_path}${id}`);
             break;
         default:
             console.error("Error: Unknown Entry Type");
