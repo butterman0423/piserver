fcdf05f8be8b5052c5523b304975a1d564e22226
Nathaniel Escaro <butterman423@gmail.com>
2024-02-19 14:18:17 -0500

    Removed unused map call

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -2,8 +2,6 @@ import { EntryInfo } from "src/types/entry_types"
 import Entry from "./entry"
 
 export default function Explorer({ entries } : { entries: Array<EntryInfo> }) {
-    const entryEls = entries.map((data, idx) => <Entry key={idx} entry={data}/>)
-    
     return (
         <div>
             {entries.length ? 
