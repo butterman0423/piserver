a24bc10b871befff32404708ca7a74a3e6b2ae06
Nathaniel Escaro <butterman423@gmail.com>
2024-02-27 00:05:42 -0500

    Replaced nested promise waiter with events.once

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import { EntryInfo, QueryRow, ENTRY_TYPE } from "./types/entry_types";
 
 import { Database } from "sqlite3";
-import { EventEmitter } from "node:events"
+import { EventEmitter, once } from "node:events"
 
 const db: Database = new Database("../res/explorer.db");
 
@@ -9,7 +9,8 @@ export async function query(tbl: string) : Promise<EntryInfo[]> {
     const waiter = new EventEmitter();
 
     const [data, _] = await Promise.all([
-        new Promise((res: (value: EntryInfo[]) => void) => waiter.once('done', res)),
+        //new Promise((res: (value: EntryInfo[]) => void) => waiter.once('done', res)),
+        once(waiter, 'done'),
         () => {
             db.serialize(() => {
                 db.all("SELECT * FROM ?", tbl, (err: Error | null, rows: any[]) => {
