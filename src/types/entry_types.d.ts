20953f1a54820d718a471ebc150e3654605a4123
Nathaniel Escaro <butterman423@gmail.com>
2024-02-26 23:57:37 -0500

    Added type for query result

--- src/types/entry_types.d.ts
+++ src/types/entry_types.d.ts
@@ -2,6 +2,13 @@ export enum ENTRY_TYPE {
     UNKNOWN, DIRECTORY, TEXT, VIDEO
 }
 
+export type QueryRow = {
+    fid: number,
+    name: string,
+    extension: string,
+    is_dir: number
+}
+
 export interface EntryInfo {
     id: number          // db row id
     type: ENTRY_TYPE
