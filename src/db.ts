8f85d0b16b238146ff50ee05b867c36fb883998e
Nathaniel Escaro <butterman423@gmail.com>
2024-02-26 23:57:51 -0500

    Added types

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,32 @@
-import { Database } from "sqlite3";
+import { EntryInfo, QueryRow, ENTRY_TYPE } from "./types/entry_types";
+
+import { Database, RunResult } from "sqlite3";
 import { EventEmitter } from "node:events"
 
 const db: Database = new Database("../res/explorer.db");
 
-export async function query(tbl: string) : Promise<unknown[]> {
+export async function query(tbl: string) : Promise<EntryInfo[]> {
     const waiter = new EventEmitter();
 
-    return await Promise.all([
-        new Promise(res => waiter.once('done', res)),
+    const [data, _] = await Promise.all([
+        new Promise((res: (value: EntryInfo[]) => void) => waiter.once('done', res)),
         () => {
             db.serialize(() => {
-                db.all("SELECT * FROM ?", tbl, (err, rows) => {
-                    waiter.emit('done', rows);
+                db.all("SELECT * FROM ?", tbl, (err: Error | null, rows: any[]) => {
+                    const entries = rows.map((row: QueryRow) : EntryInfo => 
+                        ({
+                            id: row.fid,
+                            name: row.name,
+                            extension: row.extension,
+                            type: ENTRY_TYPE.UNKNOWN        // Placeholder for now
+                        })
+                    );
+
+                    waiter.emit('done', entries);
                 })
             })
         }
     ])
+
+    return data;
 }
