619359a52ed36b47f1a027d92b9dedc5cef4382b
Nathaniel Escaro <butterman423@gmail.com>
2024-02-27 18:40:56 -0500

    Replaced getIcon function with ENTRY_ICON_MAP

--- app/routes/$/entry.tsx
+++ app/routes/$/entry.tsx
@@ -1,4 +1,5 @@
 import { ENTRY_TYPE, EntryInfo } from "src/types/entry_types";
+import { ENTRY_ICON_MAP } from "src/entry_maps";
 import strings from "res/strings.json";
 
 function getDestURL(entry: EntryInfo) : string {
@@ -14,34 +15,10 @@ function getDestURL(entry: EntryInfo) : string {
     }
 }
 
-function getIconInfo(entry: EntryInfo) : {img_res: string, alt: string} {
-    const { type } = entry;
-    let img_res, alt : string;
-    switch(type) {
-        case ENTRY_TYPE.DIRECTORY:
-            img_res = strings.img_directory;
-            alt = strings.img_alt_directory;
-            break;
-        case ENTRY_TYPE.TEXT:
-            img_res = strings.img_text;
-            alt = strings.img_alt_text;
-            break;
-        case ENTRY_TYPE.VIDEO:
-            img_res = strings.img_video;
-            alt = strings.img_alt_text;
-            break;
-        default:
-            img_res = strings.img_unknown;
-            alt = strings.img_alt_unknown;
-    }
-
-    return {img_res: img_res, alt: alt};
-}
-
 export default function Entry({ entry }: { entry: EntryInfo }) {
     const { name, extension } = entry;
     const url = getDestURL(entry);
-    const { img_res, alt } = getIconInfo(entry);
+    const { img_res, alt } = ENTRY_ICON_MAP[entry.type];
     
     return (
         <a href={url}>
