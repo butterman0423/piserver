8e167bfaa5ce71d3a3bd57efcb95812f636833b7
Nathaniel Escaro <butterman423@gmail.com>
2024-02-26 23:07:18 -0500

    Removed db.close() in query. Syntax fix

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,11 @@
 import { Database } from "sqlite3";
 
-const db: Database = new Database("../res/explorer.db")
+const db: Database = new Database("../res/explorer.db");
 
-export async function query(tbl: string) {
+export function query(tbl: string) {
     db.serialize(() => {
         db.all("SELECT * FROM (?)", tbl, (err, rows) => {
 
-        })
+        });
     });
-
-    db.close();
 }
