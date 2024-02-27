cafb44b3871485dab59a22a036c206d9c387599f
Nathaniel Escaro <butterman423@gmail.com>
2024-02-26 23:22:04 -0500

    Query can (should) now be used with async/await

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,19 @@
 import { Database } from "sqlite3";
+import { EventEmitter } from "node:events"
 
 const db: Database = new Database("../res/explorer.db");
 
-export function query(tbl: string) {
-    db.serialize(() => {
-        db.all("SELECT * FROM (?)", tbl, (err, rows) => {
+export async function query(tbl: string) : Promise<unknown[]> {
+    const waiter = new EventEmitter();
 
-        });
-    });
+    return await Promise.all([
+        new Promise(res => waiter.once('done', res)),
+        () => {
+            db.serialize(() => {
+                db.all("SELECT * FROM ?", tbl, (err, rows) => {
+                    waiter.emit('done', rows);
+                })
+            })
+        }
+    ])
 }
