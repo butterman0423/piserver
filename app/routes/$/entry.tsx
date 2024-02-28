0f9a9b1152adb6a679ec4db29263408fe048b1eb
Nathaniel Escaro <butterman423@gmail.com>
2024-02-27 19:18:05 -0500

    Modified Link destination URL

--- app/routes/$/entry.tsx
+++ app/routes/$/entry.tsx
@@ -1,28 +1,13 @@
-import { ENTRY_TYPE, EntryInfo } from "src/types/entry_types";
+import type { EntryInfo } from "src/types/entry_types";
 import { ENTRY_ICON_MAP } from "src/entry_maps";
-import strings from "res/strings.json";
 import { Link } from "@remix-run/react";
 
-function getDestURL(entry: EntryInfo) : string {
-    const {id, type} = entry;
-    switch(type) {
-        case ENTRY_TYPE.DIRECTORY:
-            return `${strings.path_explorer_path}${id}`;
-        case ENTRY_TYPE.UNKNOWN:
-            console.error(strings.error_unknown_entry);
-            return "";
-        default:
-            return `${strings.path_file_path}${id}`;
-    }
-}
-
 export default function Entry({ entry }: { entry: EntryInfo }) {
-    const { name, extension } = entry;
-    const url = getDestURL(entry);
+    const { id, name, extension } = entry;
     const { img_res, alt } = ENTRY_ICON_MAP[entry.type];
     
     return (
-        <Link to={url}>
+        <Link to={{pathname: ".", search: id.toString()}} relative="path">
             <div>
                 <img
                     src={img_res}
