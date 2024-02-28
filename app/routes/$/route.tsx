d7681b016b86d8ae068cd81f6b333f15dcc8e7b6
Nathaniel Escaro <butterman423@gmail.com>
2024-02-27 20:33:28 -0500

    Added file display framework on search query

--- app/routes/$/route.tsx
+++ app/routes/$/route.tsx
@@ -1,23 +1,49 @@
-import type { LoaderFunctionArgs } from "@remix-run/node"
+import type { LoaderFunctionArgs, TypedResponse } from "@remix-run/node"
+import type { EntryInfo } from "src/types/entry_types"
 
 import Entry from "./entry"
 import { query } from "src/db";
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
 
-export const loader = async ({ params } : LoaderFunctionArgs) => {
+type LoadResult = {
+    file_query: boolean,
+    res: EntryInfo[] | null,
+}
+
+export const loader = async ({ params, request } : LoaderFunctionArgs) : Promise<TypedResponse<LoadResult>> => {
     const path = params["*"];
 
     if(path === undefined) {
         throw "File path is undefined!";
     }
 
-    return json( await query(path) );
+    const url = new URL(request.url)
+    const fid = url.searchParams.get("file");
+
+    if(fid === null) {
+        return json({
+            file_query: false,
+            res: await query(path)
+        });
+    }
+
+    return json({
+        file_query: true,
+        res: null
+    });
 }
 
 export default function Explorer() {
-    const entries = useLoaderData<typeof loader>()
+    const { file_query, res } = useLoaderData<typeof loader>()
     
+    if(file_query) {
+        return (
+            <p>To be implemented</p>
+        )
+    }
+
+    const entries = res as EntryInfo[]
     return (
         <div>
             {entries.length ? 
