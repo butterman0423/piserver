afe0a12b58bdaf1df4fa6eec0e28157143bddf12
Nathaniel Escaro <butterman423@gmail.com>
2024-03-10 17:47:51 -0400

    Refactored db access and execution

--- src/db.ts
+++ src/db.ts
@@ -1,36 +1,42 @@
 import { EntryInfo, QueryRow, ENTRY_TYPE } from "./types/entry_types";
 
-import { Database } from "sqlite3";
+import { Database, Statement } from "sqlite3";
 import { EventEmitter, once } from "node:events"
+
+type DBCallback = (waiter: EventEmitter, err: Error | null, rows: QueryRow[]) => void;
+const SQL_DONE = 'done';
 
 const db: Database = new Database("../res/explorer.db");
 
-export async function query(tbl: string) : Promise<EntryInfo[]> {
+async function run(stmt: Statement, clbk: DBCallback): Promise<any[]> {
     const waiter = new EventEmitter();
 
-    const [data, _] = await Promise.all([
-        once(waiter, 'done'),
-        () => {
-            db.serialize(() => {
-                db.all("SELECT * FROM ?", tbl, (err: Error | null, rows: QueryRow[]) => {
-                    if(err) {
-                        throw err;
-                    }
+    const [res, _] = await Promise.all([
+        once(waiter, SQL_DONE),
+        stmt.all((err, rows: QueryRow[]) => clbk(waiter, err, rows))
+    ]);
+    
+    return res;
+}
 
-                    const entries = rows.map((row: QueryRow) : EntryInfo => 
-                        ({
-                            id: row.fid,
-                            name: row.name,
-                            extension: row.extension,
-                            type: ENTRY_TYPE.UNKNOWN        // Placeholder for now
-                        })
-                    );
-
-                    waiter.emit('done', entries);
-                })
-            })
+export async function query(tbl: string) : Promise<EntryInfo[]> {
+    const stmt: Statement = db.prepare("SELECT * FROM ?", tbl);
+    const res = await run(stmt, (waiter, err, rows) => {
+        if(err) {
+            throw err;
         }
-    ])
 
-    return data as EntryInfo[];
+        const entries = rows.map((row: QueryRow) : EntryInfo => 
+            ({
+                id: row.fid,
+                name: row.name,
+                extension: row.extension,
+                type: ENTRY_TYPE.UNKNOWN        // Placeholder for now
+            })
+        );
+
+        waiter.emit(SQL_DONE, entries);
+    });
+
+    return res as EntryInfo[]
 }
