b2529c5a5fcd335e05047afa92026053fe7c8ed4
Nathaniel Escaro <butterman423@gmail.com>
2024-03-14 19:22:08 -0400

    Added css styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,11 @@
+import { LinksFunction } from "@remix-run/node";
 import { Links, Outlet } from "@remix-run/react";
+
+import styles from "app/styles.css";
+
+export const links: LinksFunction = () => [
+    { rel: "stylesheet", href: styles }
+]
 
 export default function App() {
     return (
