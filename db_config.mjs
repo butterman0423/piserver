0f57082d71c2115e3cf0318650066b1a4ba7efc5
Nathaniel Escaro <butterman423@gmail.com>
2024-03-17 20:47:41 -0400

    Fixed buffer parsing. Rescoped db inserts

--- db_config.mjs
+++ db_config.mjs
@@ -4,12 +4,12 @@
  * RUN configure.sh TO CONFIGURE THIS SERVER'S DATABASE.
  */
 
-import { lstat } from "node:fs/promises"
+import { lstat } from "node:fs"
 import sqlite3 from 'sqlite3'
 import dbstrings from './res/db_strings.json' with { type: "json" }
 
 const { Database } = sqlite3
-let id = 1;
+const REGEX_NL = /\n|\r|\r\n/;
 
 function get_ftype(stat, name) {
     if( stat.isDirectory() ) {
@@ -48,19 +48,38 @@ async function add_entry(db, path) {
     });
 }
 
-// Create new database file
-const db = new Database("./res/explorer.db", err => { if(err) throw err });
-
 process.stdin.once('data', buffer => {
-    const data = buffer.toString('ascii').split("\n");
-
+    // Create new database file
+    const db = new Database("./res/explorer.db", err => { if(err) throw err });
+    const data = buffer.toString()
+        .split(REGEX_NL)
+        .filter(v => v);
+    
+    let id = 1;
     db.serialize(async () => {
         db.run(dbstrings.CREATE_FILE_TABLE);
         db.run(dbstrings.CREATE_FINFO_TABLE);
         
-        data.forEach(async path => await add_entry(db, path));
+        data.forEach(path => {
+            const stat = lstat(path);
+            const idx = path.lastIndexOf("/");
+
+            const pdir = path.substring(0, idx);
+            const fname = path.substring(idx);
+
+            db.run(dbstrings.INSERT_FINFO, {
+                name: fname,
+            });
+            db.run(dbstrings.INSERT_FILE, {
+                name: fname,
+                path: path,
+                parent_path: pdir,
+                type: get_ftype(stat, fname),
+                info_id: id++
+            });
+        });
 
         console.log("Configuration successful.")
         db.close();
-    })
-})
+    });
+});
